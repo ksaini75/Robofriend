e94ea8bbe19df59ca291b07a2cb0f886cd51569a
Karishma Saini <ksaini7595@gmail.com>
2020-09-26 00:54:27 -0400

    Updates

---
+++ precache-manifest.2327379d4936dfbfced838a8e5ecc6ab.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "057f8c3efe1dd1d2a38ec1d903378934",
+    "url": "/Robofriend/index.html"
+  },
+  {
+    "revision": "939c29a12718c22b5acf",
+    "url": "/Robofriend/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "2ebf874e5b49a8f286ad",
+    "url": "/Robofriend/static/css/main.608f273a.chunk.css"
+  },
+  {
+    "revision": "939c29a12718c22b5acf",
+    "url": "/Robofriend/static/js/2.e7e24cc0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Robofriend/static/js/2.e7e24cc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ebf874e5b49a8f286ad",
+    "url": "/Robofriend/static/js/main.6a84e4ee.chunk.js"
+  },
+  {
+    "revision": "73db2ff90784cbcaf08a",
+    "url": "/Robofriend/static/js/runtime-main.dc3bd113.js"
+  }
+]);
